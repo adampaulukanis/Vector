a0eacc0ddfac8014e23b02dd1d3cfd78185db0b9
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2019-05-19 16:13:03 +0100

    Now vectors are multi dimensional?
    new Vector([1, 1]) means create a new vector which points to the point
    (1, 1).
    
    Also define plus() and minus() methods.
    And length property which gives to length from the point to the point 0.

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 'use strict'
 
 module.exports = class Vector {
-  constructor (x, y) {
-    this.x = x
-    this.y = y
-    this.length = Math.sqrt(x * x + y * y) // Pythagorean theorem
+  constructor (...dimensions) {
+    this._dimensions = dimensions
+    this.length = Vector.getLength(this)
+  }
+
+  /*
+   * Returns distance from where you are to point 0
+   *
+   * Question: What do I want to see when vector is empty? I mean new Vector()
+   */
+  static getLength (vector) {
+    let length = 0
+    if (vector._dimensions.length === 0) return 0
+    if (vector._dimensions.length === 1) return vector._dimensions[0]
+
+    vector._dimensions.forEach((el) => {
+      length += ((0 - el) * (0 - el))
+    })
+    return Math.sqrt(length)
   }
 
   plus (vector) {
-    return new Vector(this.x + vector.x, this.y + vector.y)
+    let dthis = this._dimensions.length
+    let dvector = vector._dimensions.length
+    let length = dthis > dvector ? dthis : dvector
+    let sum = Array(length).fill(0)
+    sum.forEach((i, index) => {
+      sum[index] = ~~this._dimensions[index] + ~~vector._dimensions[index]
+    })
+    return sum
   }
 
   minus (vector) {
-    return new Vector(this.x - vector.x, this.y - vector.y)
+    let dthis = this._dimensions.length
+    let dvector = vector._dimensions.length
+    let length = dthis > dvector ? dthis : dvector
+    let diff = Array(length).fill(0)
+    diff.forEach((i, index) => {
+      diff[index] = ~~this._dimensions[index] - ~~vector._dimensions[index]
+    })
+    return diff
   }
 }
--- tests/testVector.js
+++ tests/testVector.js
@@ -1,50 +1,107 @@
+// TODO: n-dimension
 'use strict'
 
 const assert = require('assert')
 const Vector = require('../')
 
-let vector = null
+describe('Vector class, constructor', function () {
+  describe('new Vector()', function () {
+    const tests = new Map()
+    tests.set(() => new Vector() instanceof Vector, true)
+    tests.set(() => Array.isArray(new Vector()._dimensions), true)
+    tests.set(() => new Vector()._dimensions.length, 0)
+    tests.set(() => new Vector().length, Math.sqrt(0))
 
-beforeEach(function () {
-  vector = new Vector(1, 2)
-})
-
-describe('Vector class', function () {
-  describe('Constructor', function () {
-    it('should create a right object', function () {
-      assert(vector instanceof Vector)
-      assert.deepStrictEqual(vector.x, 1)
-      assert.deepStrictEqual(vector.y, 2)
-      assert.deepStrictEqual(vector.length, Math.sqrt(5))
-    })
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)}`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
   })
 
-  describe('plus', function () {
-    it('should return a new vector that has the sum of the two vectors', function () {
-      let testVector = new Vector(2, 3)
-      let result = vector.plus(testVector)
-      assert.deepStrictEqual(result instanceof Vector, true, 'should be an instance of Vector class')
-      assert.deepStrictEqual(result.x, 3)
-      assert.deepStrictEqual(result.y, 5)
-    })
+  describe('Vector(1, 1)', function () {
+    const tests = new Map()
+    const vector = new Vector(1, 1)
+    tests.set(() => vector._dimensions.length, 2)
+    tests.set(() => vector.length, Math.sqrt(2))
+
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)} === ${t[1]}`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
   })
 
-  describe('minus', function () {
-    it('should return a new vector that has the difference of the two vectors', function () {
-      let testVector = new Vector(2, 3)
-      let result = vector.minus(testVector)
-      assert.deepStrictEqual(result instanceof Vector, true)
-      assert.deepStrictEqual(result.x, -1)
-      assert.deepStrictEqual(result.y, -1)
-    })
+  describe('Vector(1, 1, 1)', function () {
+    const tests = new Map()
+    const vector = new Vector(1, 1, 1)
+    tests.set(() => vector._dimensions.length, 3)
+    tests.set(() => vector.length, Math.sqrt(3))
+
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)} === ${t[1]}`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
   })
 
-  describe('property length', function () {
-    it('should return the distance of the point (3, 4) from the origin (0, 0)', function () {
-      assert.deepStrictEqual(new Vector(3, 4).length, 5)
-    })
-    it('should return the distance of the point (0, 0) from the origin (0, 0)', function () {
-      assert.deepStrictEqual(new Vector(0, 0).length, 0)
-    })
+  describe('length: should return the distance of the point from the origin (0)', function () {
+    const tests = new Map()
+    tests.set(() => (new Vector().length), 0)
+    tests.set(() => (new Vector(0).length), 0)
+    tests.set(() => (new Vector(0, 0).length), 0)
+    tests.set(() => (new Vector(1).length), 1)
+    tests.set(() => (new Vector(1, 1).length), Math.sqrt(2))
+    tests.set(() => (new Vector(3, 4).length), Math.sqrt(25))
+    tests.set(() => (new Vector(1, 1, 1).length), Math.sqrt(3))
+    tests.set(() => (new Vector(1, 1, 1, 1).length), Math.sqrt(4))
+    tests.set(() => (new Vector(1, 1, 1, 1, 1).length), Math.sqrt(5))
+    tests.set(() => (new Vector(1, 1, 1, 1, 1, 1).length), Math.sqrt(6))
+    tests.set(() => (new Vector(1, 1, 1, 1, 1, 1, 1).length), Math.sqrt(7))
+
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)} === ${t[1]}`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
+  })
+
+  describe('plus: returns a new vector that has the sum/difference of the two vectors', function () {
+    const tests = new Map()
+    tests.set(() => (new Vector(1)).plus(new Vector(5)), [6])
+    tests.set(() => (new Vector(1, 1)).plus(new Vector(1, 1)), [2, 2])
+    tests.set(() => (new Vector(1, 1, 1)).plus(new Vector(2, 3, 4)), [3, 4, 5])
+    tests.set(() => (new Vector(1)).plus(new Vector(1, 1)), [2, 1])
+    tests.set(() => (new Vector(0)).plus(new Vector(10)), [10])
+    tests.set(() => (new Vector(1, 1, 1, 1)).plus(new Vector(1, 2, 3, 4, 5)), [2, 3, 4, 5, 5])
+    tests.set(() => (new Vector()).plus(new Vector()), [])
+    tests.set(() => (new Vector(1)).plus(new Vector()), [1])
+    tests.set(() => (new Vector()).plus(new Vector(1)), [1])
+
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)} === [${t[1]}]`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
+  })
+
+  describe('minus: returns a new vector that has the difference of the two vectors', function () {
+    const tests = new Map()
+    tests.set(() => (new Vector(1)).minus(new Vector(5)), [-4])
+    tests.set(() => (new Vector(1, 1)).minus(new Vector(1, 1)), [0, 0])
+    tests.set(() => (new Vector(2, 3, 4)).minus(new Vector(1, 1, 1)), [1, 2, 3])
+    tests.set(() => (new Vector(1, 0)).minus(new Vector(1)), [0, 0])
+    tests.set(() => (new Vector(0)).minus(new Vector(10)), [-10])
+    tests.set(() => (new Vector(1, 2, 3, 4, 5)).minus(new Vector(1, 1, 1, 1, 1)), [0, 1, 2, 3, 4])
+    tests.set(() => (new Vector()).minus(new Vector()), [])
+    tests.set(() => (new Vector(1)).minus(new Vector()), [1])
+    tests.set(() => (new Vector()).minus(new Vector(1)), [-1])
+    tests.set(() => (new Vector(3, 2)).minus(new Vector(-1, 3)), [4, -1])
+
+    for (const t of tests) {
+      it(`${t[0].toString().slice(6)} === [${t[1]}]`, function () {
+        assert.deepStrictEqual(t[0](), t[1])
+      })
+    }
   })
 })
