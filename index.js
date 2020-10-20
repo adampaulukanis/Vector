c5ca262478f956960f320dd696c16e2eb4f2ae62
Adam Paulukanis <adam.paulukanis@gmail.com>
2020-10-21 01:19:58 +0200

    add new functions

--- index.js
+++ index.js
@@ -2,11 +2,54 @@
 
 /**
  * Represents a 2D vector.
+ *
+ * Thanks to https://www.mathsisfun.com/algebra/vectors.html
  */
 export default class Vector {
+	#data = [];
 	constructor (x, y) {
-		/* perhaps these two below should be privat? */
-		this.x = x;
-		this.y = y;
+		this.#data[0] = x;
+		this.#data[1] = y;
+	}
+
+	get x(){
+		return this.#data[0];
+	}
+
+	get y(){
+		return this.#data[1];
+	}
+
+	static add(a, b){
+		return new Vector(a.x + b.x, a.y + b.y);
+	}
+
+	static sub(a, b){
+		/* perhaps this will be a problem. We say, in English, subtract b from a or subtract a from b? We could say both actually ;^) */
+		return new Vector(a.x - b.x, a.y - b.y);
+	}
+
+	sub(other){
+		this.#data[0] -= other.x;
+		this.#data[1] -= other.y;
+		return this;
+		this.#data.forEach((_, i) => this.#data[i] -= other[i]);
+	}
+
+	get mag(){
+		return Math.sqrt(this.#data[0] * this.#data[0] + this.#data[1] * this.#data[1]);
+	}
+
+	toString(){
+		let output = [];
+		output.push('{');
+		[...'xy'].forEach((l, i) => output.push(`${l}: ${this.#data[i]}, `));
+		output.push('}');
+		return output.join('');
+	}
+
+	mult(scalar){
+		this.#data.forEach((_, i) => this.#data[i] *= scalar);
+		return this;
 	}
 }
--- testVector.js
+++ testVector.js
@@ -8,5 +8,67 @@ describe('Vector class', function () {
     it('new Vector() works', function () {
       assert(new Vector() instanceof Vector);
     });
+    it('gets x', () => {
+      assert(new Vector(11, 12).x === 11);
+    });
+    it('gets y', () => {
+      assert(new Vector(11, 12).y === 12);
+    });
+    it('fails to set x directly', () => {
+      assert.throws(
+        () => {
+          new Vector(1, 2).x = 3;
+        },
+        /^TypeError: Cannot set property x of/
+      );
+    });
+    it('fails to set y directly', () => {
+      assert.throws(
+        () => {
+          new Vector(1, 2).y = 3;
+        },
+        /^TypeError: Cannot set property y of/
+      );
+    });
+  });
+
+  describe('adding vectors', function(){
+    it('two independent vectors (8, 13) and (26, 7) add up to the vector (34, 20)', function(){
+      const a = new Vector(8, 13);
+      const b = new Vector(26, 7);
+      const c = Vector.add(a, b);
+      assert(c.x === 34 && c.y === 20);
+    });
+  });
+
+  describe('subtracting vectors', function(){
+    it('subtract two independent vectors', function(){
+      const k = new Vector(4, 5);
+      const v = new Vector(12, 2);
+      const c = Vector.sub(v, k);
+      assert(c.x === 8 && c.y === -3);
+    });
+    it('subtract one vector from another', function(){
+      const k = new Vector(4, 5);
+      const v = new Vector(12, 2);
+      const c = v.sub(k);
+      assert(c.x === 8 && c.y === -3);
+    });
+  });
+
+  describe('magnitude of a vector', function(){
+    it('what is the magnitude of the vector b = (6, 8)', function(){
+      const b = new Vector(6, 8);
+      assert(b.mag === Math.sqrt(6 * 6 + 8 * 8)); // = √100 = 10
+    });
+  });
+
+  describe('multiplying a vector by a scalar', function(){
+    it('multiply the vector m = (7, 3) by the scalar 3', function(){
+      const m = new Vector(7, 3);
+      m.mult(3);
+      assert(m.x === 21);
+      assert(m.y === 9);
+    });
   });
 });
